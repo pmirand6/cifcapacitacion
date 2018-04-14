e2953d941bfd73dd79c322174b06e203569f9f8c
arboles2912 <pmirand6@gmail.com>
2018-04-13 23:14:45 -0300

    desde el laburo

--- test2.php
+++ test2.php
@@ -1,46 +1,50 @@
-<!DOCTYPE html> 
-<html lang="es" ""> 
-<head><meta http-equiv="Content-Type" content="text/html; charset=euc-jp"> 
-	
-	<meta http-equiv="X-UA-Compatible" content="IE=edge"> 
-	<meta name="viewport" content="width=device-width, initial-scale=1.0"> 
-	<meta name="google-site-verification" content="nHrRStWkMH1mRyhmlAZh8I9JY1CDiWyNN03pp2UNkog" /> 
-	<meta name="google-site-verification" content="nHrRStWkMH1mRyhmlAZh8I9JY1CDiWyNN03pp2UNkog" /> 
-	<meta name="keywords" content="CIFcapacitacion, centro integral de formacion, tigre, pacheco, Mecanica dental, farmacia, asistente, salud, benavidez, talar,CIF, cif, cif capacitacion, argentina, buenos aires" /> .
-	<meta name="description" content="En nuestro sitio encontraras todos los cursos que necesitas para construir tu futuro laboral." /> 
-	<link rel="shortcut icon" href="ico/favicon.ico"> 
-	<title>CIF Centro Integral de Formacion</title>         
-	<!-- Bootstrap core CSS -->         
-	<link href="bootstrap/css/bootstrap.css" rel="stylesheet"> 
-	<!-- Custom styles for this template -->         
-	<link href="css/style.css" rel="stylesheet"> 
-	<link href="css/institucional.css" rel="stylesheet"> 
+<!DOCTYPE html>
+<html lang="es" "">
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=euc-jp">
+
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="google-site-verification" content="nHrRStWkMH1mRyhmlAZh8I9JY1CDiWyNN03pp2UNkog" />
+	<meta name="google-site-verification" content="nHrRStWkMH1mRyhmlAZh8I9JY1CDiWyNN03pp2UNkog" />
+	<meta name="keywords" content="CIFcapacitacion, centro integral de formacion, tigre, pacheco, Mecanica dental, farmacia, asistente, salud, benavidez, talar,CIF, cif, cif capacitacion, argentina, buenos aires"
+	/> .
+	<meta name="description" content="En nuestro sitio encontraras todos los cursos que necesitas para construir tu futuro laboral."
+	/>
+	<link rel="shortcut icon" href="ico/favicon.ico">
+	<title>CIF Centro Integral de Formacion</title>
+	<!-- Bootstrap core CSS -->
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+	<!-- Custom styles for this template -->
+	<link href="css/style.css" rel="stylesheet">
 	<link href="https://cdn.rawgit.com/michalsnik/aos/2.1.1/dist/aos.css" rel="stylesheet">
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->         
-        <!--[if lt IE 9]>
+	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+	<!--[if lt IE 9]>
       <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
       <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-  <![endif]-->         
-  <link rel="stylesheet" href="components/pg.blocks/css/blocks.css"> 
-  <link rel="stylesheet" href="components/pg.blocks/css/plugins.css"> 
-  <link rel="stylesheet" href="components/pg.blocks/css/style-library-1.css"> 
-  <link rel="stylesheet" href="components/pg.blocks/css/gallery.css"> 
-  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700"> 
-  <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic"> 
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css"> 
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-    
-</head>     
-<body> 
+  <![endif]-->
+	<link rel="stylesheet" href="components/pg.blocks/css/blocks.css">
+	<link rel="stylesheet" href="components/pg.blocks/css/plugins.css">
+	<link rel="stylesheet" href="components/pg.blocks/css/style-library-1.css">
+	<link rel="stylesheet" href="components/pg.blocks/css/gallery.css">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,400,300,600,700">
+	<link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:300,400,700,300italic,400italic,700italic">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.6.3/css/font-awesome.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
+</head>
+
+<body>
 	<?php include "./navs.php";?>
-	
+
 	<section>
 
 		<div class="container gallery-container">
 			<h3>Nuestros Cursos</h3>
 			<div class="tz-gallery">
-                <p class="page-description text-center">Cursos de Salud</p>
+				<p class="page-description text-center">Cursos de Salud</p>
 				<div class="row">
 					<div class="col-sm-6 col-md-4">
 						<div class="thumbnail">
@@ -49,7 +53,9 @@
 							</a>
 							<div class="caption">
 								<h3>Mecanica Dental</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+									aliqua.
+								</p>
 							</div>
 						</div>
 					</div>
@@ -60,7 +66,9 @@
 							</a>
 							<div class="caption">
 								<h3>Auxiliar de Farmacia</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+									aliqua.
+								</p>
 							</div>
 						</div>
 					</div>
@@ -71,47 +79,24 @@
 							</a>
 							<div class="caption">
 								<h3>Asistente Dental</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
+								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+									aliqua.
+								</p>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-        </div>
-        <div class="container gallery-container text-center">
-			<div class="tz-gallery">
-                <p class="page-description text-center">Cursos de Gastronomia</p>
-				<div class="row">
-					<div class="col-sm-6 col-md-4">
-                        <div class="center-block">
-                        <div class="thumbnail">
-							<a class="lightbox" href="images/mecanica.png">
-								<img src="https://www.meldmagazine.com.au/wp-content/uploads/2016/11/meld-waitress-waiter-service-hospitality-2.jpg" alt="Park">
-							</a>
-							<div class="caption">
-								<h3>Mozo</h3>
-								<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-							</div>
-						</div>
-                        </div>
-					</div>
-					<div class="col-sm-6 col-md-4">
-					</div>
-					<div class="col-sm-6 col-md-4">
-					</div>
-				</div>
-			</div>
-		</div>  
+		</div>
 	</section>
-	
-	
+
+
 	<script src="bootstrap/js/bootstrap.min.js"></script>
-	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->         
-	<script src="assets/js/ie10-viewport-bug-workaround.js"></script>         
+	<!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+	<script src="assets/js/ie10-viewport-bug-workaround.js"></script>
 	<script>
-        
-    </script>
-    
+	</script>
+
 </body>
 
 </html>
